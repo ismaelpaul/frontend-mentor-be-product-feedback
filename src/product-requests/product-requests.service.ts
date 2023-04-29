f7672bf428336f99ff13b71541df429e33a782c6
ismaelpaul <ismaafp@gmail.com>
2023-04-29 22:21:19 +0100

    Add findById ProductRequestService method

--- src/product-requests/product-requests.service.ts
+++ src/product-requests/product-requests.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { ProductRequests } from './schemas/product-requests.schema';
 import * as mongoose from 'mongoose';
@@ -14,5 +14,15 @@ export class ProductRequestsService {
     const productRequests = await this.productRequestsModel.find();
 
     return productRequests;
+  }
+
+  async findById(id: string): Promise<ProductRequests> {
+    const singleProductRequest = await this.productRequestsModel.findById(id);
+
+    if (!singleProductRequest) {
+      throw new NotFoundException('Product request not found!');
+    }
+
+    return singleProductRequest;
   }
 }
