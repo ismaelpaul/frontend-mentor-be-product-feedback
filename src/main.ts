6d213f33cf859dbb39932267da7a53a6f12696be
ismaelpaul <ismaafp@gmail.com>
2023-08-01 20:37:31 +0100

    Add client url

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
 
   app.use(
     cors({
-      origin: ['http://127.0.0.1:5173/', 'http://localhost:5173'],
+      origin: [
+        'https://fe-product-feedback.netlify.app',
+        'http://localhost:5173',
+      ],
       credentials: true,
     }),
   );
